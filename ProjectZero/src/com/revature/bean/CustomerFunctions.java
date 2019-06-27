b49e8c0a26b1ee55a1cf5b9cc6f2f7eb68cd2cd6
Alphonse Wilson <Alphonse.Wilson.software@gmail.com>
2019-06-27 16:48:39 -0400

    updating the case statments and funcitons on the customer side

--- ProjectZero/src/com/revature/bean/CustomerFunctions.java
+++ ProjectZero/src/com/revature/bean/CustomerFunctions.java
@@ -1,5 +1,74 @@
 package com.revature.bean;
+import java.util.Scanner;
 
+import com.revature.bean.VariableCheck;
 public class CustomerFunctions {
+	public void ViewCarLot() {
+		//displays list of available cars on lot 
+    	//from the sql table
+    	System.out.println("1.car, 2.car, 3.car");
+	}
+	public void ViewMyCars() {
+		//displays list of available cars previously purchased 
+    	//from the sql table
 
+    	System.out.println("You have no cars ");
+	}
+	public Object MakeAnOffer(VariableCheck variables) {
+		String selectionMenu = (""
+				+ "1. View cars on lot "
+				+ "\n2. View my cars"
+				+ "\n3. Make an offer "
+				+ "\n4. View remaining payment foreach of your car(s)   "
+				+ "\n5. Exit");
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Which car would you like to make an offer for?");
+    	//displays list of available cars on lot 
+    	//from the sql table
+    	System.out.println("1.car, 2.car, 3.car");
+    	while(!sc.hasNextInt()) {
+			System.out.println("Invalid input try again"+selectionMenu);
+			sc.next();
+		}
+    	variables.setCarNumber(sc.nextInt());
+    	// 
+    	//use the get method below to figure if the car actually exists in the database 
+    	// if it does then display the car using sql below.
+    	
+    	//if(car exists) {
+    		System.out.println("making an offer for "+variables.getCarNumber());
+    		System.out.println("What would you like to offer: ");
+    		System.out.print("$");
+    		while(!sc.hasNextInt()) {
+				System.out.println("Invalid input try again"+selectionMenu);
+				sc.next();
+			}
+    		variables.setOffer(sc.nextInt());
+    		System.out.println("Thankyou, your offer will viewed by an employee shortly");
+    		//use the get method to input the offer to the database
+    //	}
+    	//else{System.out.println(that car does not exist);
+			return variables;
+    	//}
+	}
+	public void ViewCarPaymentDetails() {
+		//use sql to list the remaining payment for each of the owned cars
+    	System.out.println("you own no money so far");
+	}
+	public void ResetUserValues(User user, VariableCheck variables) {
+		user.setCustomer(false);
+    	user.setEmployee(false);
+    	user.setUser(null);
+    	user.setPass(null);
+    	variables.setLogin(false);
+    	
+    	
+    	System.out.println("something went wrong."
+    			+ " please enter your selected options correctly");
+		
+	}
+
+		
+	
 }
--- ProjectZero/src/com/revature/menu/CustomerMenu.java
+++ ProjectZero/src/com/revature/menu/CustomerMenu.java
@@ -1,14 +1,15 @@
 package com.revature.menu;
 
 import java.util.Scanner;
-
+import com.revature.bean.CustomerFunctions;
 import com.revature.bean.User;
 import com.revature.bean.VariableCheck;
 
 public class CustomerMenu{
 	public User LoginCustomer(User user) {
 		VariableCheck variables = new VariableCheck();
-		
+		CustomerFunctions customer = new CustomerFunctions();
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		variables.setLoop(true);
 		//variables.setLogin(false);
@@ -19,8 +20,6 @@ public class CustomerMenu{
 				+ "\n4. View remaining payment foreach of your car(s)   "
 				+ "\n5. Exit");
 		while(variables.getLoop() == true) {
-			
-		//	if(variables.getLogin() == false) {
 				
 				System.out.println(selectionMenu);
 				while(!sc.hasNextInt()) {
@@ -33,53 +32,16 @@ public class CustomerMenu{
 				
 				switch (variables.getMenuOption()) {
 		        case 1:
-		        	//displays list of available cars on lot 
-		        	//from the sql table
-		        	System.out.print("1.car, 2.car, 3.car");
+		        	customer.ViewCarLot();
 		            break;
 		        case 2:
-		        	//displays list of available cars previously purchased 
-		        	//from the sql table
-
-		        	System.out.print("You have no cars ");
-		        	
+		        	customer.ViewMyCars();
 		            break;
 		        case 3:
-		        	System.out.print("Which car would you like to make an offer for?");
-		        	//displays list of available cars on lot 
-		        	//from the sql table
-		        	System.out.print("1.car, 2.car, 3.car");
-		        	while(!sc.hasNextInt()) {
-						System.out.println("Invalid input try again"+selectionMenu);
-						sc.next();
-					}
-		        	variables.setCarNumber(sc.nextInt());
-		        	// 
-		        	//use the get method below to figure if the car actually exists in the database 
-		        	// if it does then display the car using sql below.
-		        	
-		        	//if(car exists) {
-		        		System.out.println("making an offer for "+variables.getCarNumber());
-		        		System.out.println("What would you like to offer: ");
-		        		System.out.print("$");
-		        		while(!sc.hasNextInt()) {
-							System.out.println("Invalid input try again"+selectionMenu);
-							sc.next();
-						}
-		        		variables.setOffer(sc.nextInt());
-		        		System.out.println("Thankyou, your offer will viewed by an employee shortly");
-		        		//use the get method to input the offer to the database
-		        //	}
-		        	//else{System.out.println(that car does not exist);
-
-		        	//}
-		        	
-		            
+		        	customer.MakeAnOffer(variables);
 		        	break;
 		        case 4:
-		        	
-		        	//use sql to list the remaining payment for each of the owned cars
-		        	System.out.println("you own no money so far");
+		        	customer.ViewCarPaymentDetails();
 		            break;
 		        case 5:
 		        	System.out.println("Exiting");
@@ -90,20 +52,13 @@ public class CustomerMenu{
 		        	
 		            break;
 		        default: 
-		        	user.setCustomer(false);
-		        	user.setEmployee(false);
-		        	user.setUser(null);
-		        	user.setPass(null);
-		        	variables.setLogin(false);
-		        	
-		        	
-		        	System.out.println("something went wrong."
-		        			+ " please enter your selected options correctly");
+		        	customer.ResetUserValues(user, variables);
 		    }
+				
 				System.out.println("Username: "+user.getUser()+
 						"\nCar: "+variables.getCarNumber()+" Offer: "+variables.getOffer()
 						+"\nMenu: "+variables.getMenuOption());
-			//}
+			
 			
 		}
 		return user;
