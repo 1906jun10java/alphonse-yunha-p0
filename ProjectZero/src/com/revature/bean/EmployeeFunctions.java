9edf9d2b0ce25dbcd34e2d1eb627bba4aef7c152
YunHa Jung <jungyunha1994@live.com>
2019-07-03 22:33:23 -0400

    Update EmployeeFunctions.java

--- ProjectZero/src/com/revature/bean/EmployeeFunctions.java
+++ ProjectZero/src/com/revature/bean/EmployeeFunctions.java
@@ -1,6 +1,30 @@
 package com.revature.bean;
 
-public class EmployeeFunctions {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.dao.LotDAO;
+import com.revature.util.ConnFactory;
+
+public class EmployeeFunctions implements LotDAO {
+	public static ConnFactory cf = ConnFactory.getInstance();
+
+	public List<Car> getCarList() throws SQLException{
+		List<Car> carList = new ArrayList<Car>();
+		Connection conn = cf.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM LOT");
+		Car s = null;
+		while(rs.next()) {
+			s = new Car(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4));
+			carList.add(s);
+		}
+		return carList;
+	}
 	public void addCarToLot(User user) {
 		System.out.println("Add Car.");
 	}
