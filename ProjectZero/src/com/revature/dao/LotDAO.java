6c133448a933f4989b1df29c6b1b3cf1b5375cbc
jungy <jungyunha1994@live.com>
2019-07-03 22:37:47 -0400

    s

---
+++ ProjectZero/src/com/revature/dao/LotDAO.java
@@ -0,0 +1,14 @@
+package com.revature.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.revature.bean.Car;
+
+public interface LotDAO {
+	
+
+	public abstract List<Car> getCarList() 
+			throws SQLException;
+
+}
