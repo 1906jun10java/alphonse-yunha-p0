c0bbac163f4f91765c86f29be9f84b6244a6fd4f
jungy <jungyunha1994@live.com>
2019-07-04 22:50:06 -0400

    changes to show table

--- ProjectZero/src/com/revature/bean/EmployeeFunctions.java
+++ ProjectZero/src/com/revature/bean/EmployeeFunctions.java
@@ -2,6 +2,7 @@ package com.revature.bean;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -10,7 +11,6 @@ import java.util.Scanner;
 
 import com.revature.dao.CarDAO;
 import com.revature.dao.CarDAOImpl;
-import com.revature.dao.LotDAO;
 import com.revature.util.ConnFactory;
 
 public class EmployeeFunctions {
@@ -23,48 +23,58 @@ public class EmployeeFunctions {
 	Scanner sc = new Scanner(System.in);
 	public static Connection cf = ConnFactory.getConnection();
 
-	public void addCarToLot(Car car)  {
-	System.out.println("Enter year manufactured: ");
-	yearManufactured = sc.nextInt();
-	
-	System.out.println("Enter car make: ");
-	carMake = sc.nextLine();
-	carMake = sc.nextLine();
-	
-	
-	System.out.println("Enter car model: ");
-	carModel = sc.nextLine();
-	
-	car = new Car(userId, yearManufactured, carMake, carModel);
-	cd.addCarToLot(car);
-		
+	public void addCarToLot(Car car) {
+		System.out.println("Enter year manufactured: ");
+		yearManufactured = sc.nextInt();
+
+		System.out.println("Enter car make: ");
+		carMake = sc.nextLine();
+		carMake = sc.nextLine();
+
+		System.out.println("Enter car model: ");
+		carModel = sc.nextLine();
+
+		car = new Car(userId, yearManufactured, carMake, carModel);
+		cd.addCarToLot(car);
+
 	}
+
 	public void removeCarFromLot(User user) {
 		System.out.println("Removed.");
 	}
+
 	public void offerDecide(User user) {
 		System.out.println("Accept/Decline");
 	}
+
 	public void viewPayments(User user) {
 		System.out.println("View payments");
 	}
-/*	public void returnToMenu(User user) {
-		System.out.println("Returned To Menu");
-	}*/
+
+	/*
+	 * public void returnToMenu(User user) { System.out.println("Returned To Menu");
+	 * }
+	 */
 	public void exitProgram(VariableCheck variable, User user) {
 		user.setTravelTo("login");
 		variable.setLoop(false);
 		System.out.println("exiting.");
-		//System.exit(0);
+		// System.exit(0);
 	}
-	public List<Car> getCarList() throws SQLException{
+
+	public List<Car> getCarList() throws SQLException {
 		List<Car> carList = new ArrayList<Car>();
 		Connection conn = ConnFactory.getConnection();
 		Statement stmt = conn.createStatement();
 		ResultSet rs = stmt.executeQuery("SELECT * FROM LOT");
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnsNumber = rsmd.getColumnCount();
 		Car s = null;
-		while(rs.next()) {
-			s = new Car(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4));
+		while (rs.next()) {
+			for (int i = 2; i <= columnsNumber; i++) {
+				System.out.print(rs.getString(i) + " ");
+			}
+			System.out.println();
 			carList.add(s);
 		}
 		return carList;
--- ProjectZero/src/com/revature/dao/CarDAOImpl.java
+++ ProjectZero/src/com/revature/dao/CarDAOImpl.java
@@ -14,24 +14,24 @@ import com.revature.util.ConnFactory;
 public class CarDAOImpl implements CarDAO {
 	public List<Car> getCar() {
 		List<Car> carList = new ArrayList<>();
-		
+
 		String sql = "SELECT * FROM LOT";
-		
-		try(Connection conn = ConnFactory.getConnection();
+
+		try (Connection conn = ConnFactory.getConnection();
 				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(sql);){
-			
-			while(rs.next()) {
+				ResultSet rs = stmt.executeQuery(sql);) {
+
+			while (rs.next()) {
 				Car c = new Car();
 				int carYear = rs.getInt("CAR_YEAR");
 				c.setYearManufactured(carYear);
-				
+
 				String carMake = rs.getString("CAR_MAKE");
 				c.setMake(carMake);
-				
+
 				String carModel = rs.getString("CAR_MODEL");
 				c.setModel(carModel);
-				
+
 				carList.add(c);
 			}
 		} catch (SQLException e) {
@@ -40,77 +40,77 @@ public class CarDAOImpl implements CarDAO {
 		}
 		return carList;
 	}
-	
+
 	public Car getCarById(String id) {
 		Car c = null;
 		String sql = "SELECT CAR_ID, CAR_YEAR, CAR_MAKE, CAR_MODEL FROM LOT WHERE CAR_YEAR = '?' AND CAR_MAKE = '?' AND CAR_MODEL = '?'";
-		
+
 		ResultSet rs = null;
-		try(Connection conn = ConnFactory.getConnection();
-				PreparedStatement ps = conn.prepareStatement(sql);){
+		try (Connection conn = ConnFactory.getConnection(); PreparedStatement ps = conn.prepareStatement(sql);) {
 			ps.setString(1, id);
 			rs = ps.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int carID = rs.getInt("CAR_ID");
 				int carYear = rs.getInt("CAR_YEAR");
 				String carMake = rs.getString("Car_MAKE");
 				String carModel = rs.getString("Car_Model");
-				
+
 				c = new Car(carID, carYear, carMake, carModel);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
-			if(rs!= null) {
+		} finally {
+			if (rs != null) {
 				try {
 					rs.close();
-				}catch (SQLException e) {
+				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 		return c;
 	}
+
 	@Override
 	public Car getCarById(String id, Connection conn) {
 		Car c = null;
 		String sql = "SELECT CAR_ID, CAR_YEAR, CAR_MAKE, CAR_MODEL FROM LOT WHERE CAR_YEAR = '?' AND CAR_MAKE = '?' AND CAR_MODEL = '?'";
-		
+
 		ResultSet rs = null;
-		try(PreparedStatement ps = conn.prepareStatement(sql);){
+		try (PreparedStatement ps = conn.prepareStatement(sql);) {
 			ps.setString(1, id);
 			rs = ps.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int carID = rs.getInt("CAR_ID");
 				int carYear = rs.getInt("CAR_YEAR");
 				String carMake = rs.getString("Car_MAKE");
 				String carModel = rs.getString("Car_Model");
-				
+
 				c = new Car(carID, carYear, carMake, carModel);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
-			if(rs!= null) {
+		} finally {
+			if (rs != null) {
 				try {
 					rs.close();
-				}catch (SQLException e) {
+				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
 		}
 		return c;
 	}
+
 	public int addCarToLot(Car car) {
 		int addedCar = 0;
 		String sql = "INSERT INTO LOT(CAR_YEAR, CAR_MAKE, CAR_MODEL) VALUES(?,?,?)";
-		try(Connection conn = ConnFactory.getConnection();
-				PreparedStatement ps = conn.prepareStatement(sql)){
-			
+		try (Connection conn = ConnFactory.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
 			ps.setInt(1, car.getYearManufactured());
 			ps.setString(2, car.getMake());
 			ps.setString(3, car.getModel());
@@ -120,7 +120,7 @@ public class CarDAOImpl implements CarDAO {
 			e.printStackTrace();
 		}
 		return addedCar;
-		
+
 	}
 
 }
